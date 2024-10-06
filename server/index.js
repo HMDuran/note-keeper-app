deab41393ea4c5cb1f0fe9d15864f402f66f0662
HMDuran <hanahmae.duran@gmail.com>
2024-10-06 15:51:56 +0800

    chore: set up React app, backend index.js, database, and environment variables

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
